6f28c31c986ea66638d6326b0da2df41e7abfe07
olni0002 <olni0002@stud.kea.dk>
2024-10-08 11:11:59 +0200

    Add page for movies

---
+++ js/moviedetails.js
@@ -0,0 +1,42 @@
+const id = new URLSearchParams(location.search).get("id");
+
+fetch(`http://127.0.0.1:8080/api/movie/${id}`).then(response => response.json())
+                                              .then(data => addData(data));
+
+function addData(data) {
+    document.getElementById("title").textContent = data.title;
+    document.getElementById("category").textContent = data.category.toLowerCase();
+    document.getElementById("description").textContent = data.description;
+
+    const dString = data.duration;
+    let durationHours;
+    let durationMinutes;
+
+    if (dString.includes("H") && dString.includes("M")) {
+        durationHours = dString.substring(dString.indexOf("T") + 1, dString.indexOf("H"));
+        durationMinutes = dString.substring(dString.indexOf("H") + 1, dString.indexOf("M"));
+    } else if (dString.includes("H")) {
+        durationHours = dString.substring(dString.indexOf("T") + 1, dString.indexOf("H"));
+        durationMinutes = 0;
+    } else if (dString.includes("M")) {
+        durationHours = 0;
+        durationMinutes = dString.substring(dString.indexOf("T") + 1, dString.indexOf("M"));
+    } else {
+        durationHours = 0;
+        durationMinutes = 0;
+    }
+
+    
+
+    document.getElementById("duration").textContent = `${durationHours}h ${durationMinutes}m`;
+    
+    const uint8 = Uint8Array.from(atob(data.image).split("").map(char => char.charCodeAt()))
+    
+    let blob = new Blob([uint8]);
+
+    let file = new File([blob], "movieimage",);
+
+    const url = URL.createObjectURL(file);
+
+    document.getElementById("image").src = url;    
+}
